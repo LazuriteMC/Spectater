848f24d6d88fd484658e706cd876193edbb20b2e
Patrick Hofmann <patrickhofmanndesigns@gmail.com>
2023-07-12 21:20:20 -0600

    Gradle and loom updates

--- src/main/java/dev/lazurite/spectater/mixin/access/ILocalPlayerMixin.java
+++ src/main/java/dev/lazurite/spectater/mixin/access/ILocalPlayerMixin.java
@@ -1,6 +1,5 @@
 package dev.lazurite.spectater.mixin.access;
 
-
 import net.minecraft.client.player.LocalPlayer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Invoker;
